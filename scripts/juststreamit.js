848101df95e4a08e55b81a501c3fb699c40a5126
Fovea4F <bernard.rouvreau@free.fr>
2023-10-10 19:37:56 +0200

    Validation finale

--- scripts/juststreamit.js
+++ scripts/juststreamit.js
@@ -11,35 +11,52 @@ const titlesSort = "-imdb_score";
 const categoryList = ['numberOne', 'bestFilms', 'Biography', 'Sport', 'Adventure'];
 const numberOfFilms = 7;
 
-/*****************************************************************************/
-/* Sélectionnez tous les éléments avec la classe "image-modal-trigger"       */
-/*****************************************************************************/
-// Sélectionnez l'élément de la boîte modale
-
-/*****************************************************************************/
-
-function createButton(parentElement) {
-    let button = document.createElement('button');
-    button.textContent = 'Lancer la vidéo'
-}
-
 function createInformationList(imageInformation, filmData) {
-    //prepare list information to display in Modal Windows
-    
+    //create data in <ul> for modal image information
+
     console.log('Données Film : ', filmData);
-    imageInformation.id = 'imageInfo';
-    let informationList = [];
-    informationList.push([`Titre du film     :  ${filmData.title}`]);
-    informationList.push([`Genre             :  ${filmData.genres}`]);
-    informationList.push([`Année de sortie   :  ${filmData.year}`]);
-    informationList.push([`Notation          :  ${filmData.votes}`]);
-    informationList.push([`Score imdb        :  ${filmData.imdb_score}`]);
-    informationList.push([`Réalisateur       :  ${filmData.directors}`]);
-    informationList.push([`Liste des acteurs :  ${filmData.actors}`]);
-    informationList.push([`Description       :  ${filmData.description}`]);
-    informationList.push([`Durée             :  ${filmData.duration} minutes`]);
-    
-    return informationList;
+    let dataElement = document.createElement('li');
+    dataElement.textContent = `Titre du film            :  ${filmData.title}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Genre                    :  ${filmData.genres}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Année de sortie          :  ${filmData.year}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Nombre de votes          :  ${filmData.votes}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Score imdb               :  ${filmData.imdb_score}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Réalisateur              :  ${filmData.directors}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Liste des acteurs        :  ${filmData.actors}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Description              :  ${filmData.description}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Durée                    :  ${filmData.duration} minutes`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Pays d'origine           :  ${filmData.countries}`;
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    //dataElement.textContent = `Résultats du Box Office  : ${filmData.revenue} ${filmData.currency}`;
+    if (filmData.worldwide_gross_income === null) {
+        let boxOfficeRevenue = 'Non communiqué';
+        dataElement.textContent = `Résultats du Box Office  : ${boxOfficeRevenue}`;
+    } else {
+        dataElement.textContent = `Résultats du Box Office  : ${filmData.worldwide_gross_income} US$`;
+    }
+    imageInformation.appendChild(dataElement);
+    dataElement = document.createElement('li');
+    dataElement.textContent = `Résumé du film           : ${filmData.long_description}`;
+    imageInformation.appendChild(dataElement);
 };
 
 function addArrows(category) {
@@ -67,132 +84,77 @@ function addArrows(category) {
     divCarrousel.appendChild(leftArrow);
 };
 
-function addRightArrow(category) {
-    //Add arrows for Carrousel
-
-    if (category == 'numberOne') {
-        return;
-    }
-    let divCarrousel = document.getElementById(`carrousel${category}`);
-    //let divContainerImages = document.getElementById(`container${category}`);
-    let rightArrow = document.createElement('img');
-    rightArrow.src="images/icons8-chevron-droit-48.png";
-    rightArrow.alt="right arrow - Image de icon8.com";
-    rightArrow.classList.add("rightarrow");
-    rightArrow.classList.add("arrow");
-    rightArrow.classList.add("overlay");
-    rightArrow.id=`rightarrow${category}`;
-    divCarrousel.appendChild(rightArrow);
-    //divContainerImages.appendChild(rightArrow);
-};
-
-function addLeftArrow(category) {
-    //Add arrows for Carrousel
-
-    if (category == 'numberOne') {
-        return;
-    }
-    let divCarrousel = document.getElementById(`carrousel${category}`);
-    //let divContainerImages = document.getElementById(`container${category}`);
-    let leftArrow = document.createElement('img');
-    leftArrow.src="images/icons8-chevron-gauche-48.png";
-    leftArrow.alt="left arrow - Image de icon8.com";
-    leftArrow.classList.add("leftarrow");
-    leftArrow.classList.add("arrow");
-    leftArrow.classList.add("overlay");
-    leftArrow.id=`leftarrow${category}`;
-    divCarrousel.appendChild(leftArrow);
-    //divContainerImages.appendChild(leftArrow);
-};
-
 function  displayHideArrows(position, numberImage, categoryFilms) {
     //allow hide arrow at limit
 
     const leftArrow = document.getElementById(`leftarrow${categoryFilms}`)
     const rightArrow = document.getElementById(`rightarrow${categoryFilms}`)
     if (position == 0) {
-        //leftArrow.setAttribute('display', 'none');
         rightArrow.style.display = 'none';
     } else {
-        //leftArrow.setAttribute('display','block');
         rightArrow.style.display = 'block';
     };
     if (position == -numberImage+4) {
-        //rightArrow.setAttribute('display', 'none');
         leftArrow.style.display = 'none';
     } else {
-        //rightArrow.setAttribute('display','block');
         leftArrow.style.display = 'block';
     };
 };
 
 function modalClose() {
     
-    const modalDiv = document.getElementById("image-modal"); //Select Div Modal
-    const imageInModal = document.getElementById("modal-image"); //Select image inside Modal
+    const modal = document.getElementById("modalDiv"); //Select Div Modal
+    const imageInModal = document.getElementById("modalImageDiv"); //Select image inside Modal
 
-    imageInModal.setAttribute("src", "");
-    imageInModal.setAttribute("alt", "");
-    
-    while ((document.getElementById('imageInfo')) != null) {
-        let elementUl = document.getElementById('imageInfo');
-        elementUl.remove();
+    while (imageInModal.firstChild) {
+        imageInModal.removeChild(imageInModal.firstChild);
     };
-    modalDiv.style.display = "none"; // Hide modal window
+    modal.style.display = "none"; // Hide modal window
 };
 
-function modalCreation(imageDiv, filmData) {
+function modalCreate(imageDiv, filmData) {
     /*Modal Event actions*/
 
-    const modalDiv = document.getElementById("image-modal"); //Select Div Modal
-    const imageInModal = document.getElementById("modal-image"); //Select image inside Modal
-    const elementUl = document.getElementById('imageInfo');
-
-    //console.log('Affichage imageDiv modal : ', imageDiv);
+    const modalImageDiv = document.getElementById("modalImageDiv"); //Select image inside Modal
+    
     imageDiv.addEventListener("click", () => {
         //Modal windows data creation and open actions
         modalClose();
-        /* (elementUl != null) {
-            elementUl.remove();;
-        }*/
-        let imageInformation = document.createElement('ul');
-        let informationList = createInformationList(imageInformation, filmData);
-        //console.log('ul modal : ', elementUl);
-        //ul prune if exist
-        //console.log('Modal ON modal creation: ', elementUl);
-        console.log('erreur image :', filmData.image_url);
+        const imageDiv = document.createElement('div');
+        imageDiv.id = 'imageDiv';
+        modalImageDiv.appendChild(imageDiv);
+        const image = document.createElement('img');
         if (!filmData.image_url) {
-            
-            imageInModal.setAttribute("src", "images/buildings.jpg");
+            image.setAttribute("src", "images/buildings.jpg");
         } else {
-            imageInModal.setAttribute("src", filmData.image_url);
-        }
-        imageInModal.setAttribute("src", filmData.image_url);
-        imageInModal.setAttribute("alt", imageDiv.alt);    
-        for (index = 0; index<informationList.length; index++) {
-            let dataElement = document.createElement('li');
-            dataElement.textContent = informationList[index];
-            imageInformation.appendChild(dataElement);
-        };                  
-        modalDiv.appendChild(imageInformation);
-        modalDiv.classList.add('image-modal-trigger');
-        modalDiv.style.display = "block"; // Affichez la boîte modale
-        //console.log("Triggers");
-        // Modal Events close actions
-        modalDiv.addEventListener("click", () => {
-           // modalClose(imageInModal, modalDiv);
-           modalClose();
-        });
-        const closeModal = document.getElementById('close-modal');
-        closeModal.addEventListener("click", () => {
-            //modalClose(imageInModal, modalDiv);
+            image.setAttribute("src", filmData.image_url);
+        };
+        image.setAttribute("alt", imageDiv.alt);
+        imageDiv.appendChild(image);
+        const imageInfo = document.createElement('div');
+        imageInfo.id = 'imageInfo';
+        modalImageDiv.appendChild(imageInfo);
+        let imageInformation = document.createElement('ul');
+        imageInfo.appendChild(imageInformation);
+        createInformationList(imageInformation, filmData);
+        modalDiv.classList.add('modalDiv-trigger');
+        modalDiv.style.display = "block"; // modal windows display On
+        // Modal Events close actions on the whole modal Window
+        /*modalDiv.addEventListener("click", () => {
+            console.log('ligne178 modalClose');
+                modalClose();
+        });*/
+        let closeButton = document.getElementById('close-modal')
+        closeButton.addEventListener("click", () => {
+            console.log('ligne182 modalClose');
             modalClose();
         });
         window.addEventListener("click", (event) => {
+            console.log('ligne187 modalClose');
             if (event.target === modalDiv) {
                 //modalClose(imageInModal, modalDiv);
                 modalClose();
-            };
+           };
         });
     });
 };
@@ -229,7 +191,6 @@ function createCarrouselDiv(categoryFilms) {
                 //divCarrousel.setAttribute('order', 'unset');
         };
         divCarrousel.appendChild(title);
-        //addRightArrow(categoryFilms);
         addArrows(categoryFilms);
     };
 };
@@ -238,7 +199,6 @@ function createContainerDiv(categoryFilms, videoTitle) {
     // create div container that will host images of a category
 
     let divCarrousel = document.getElementById(`carrousel${categoryFilms}`);
-    //addLeftArrow(categoryFilms);
     let divContainerImages = document.createElement('div');
     divContainerImages.id = `container${categoryFilms}`;
     divContainerImages.classList.add('containerimages');
@@ -262,9 +222,6 @@ function createContainerDiv(categoryFilms, videoTitle) {
         divContainerImages.style.width=`${(182*7)}px`;
     };
     divCarrousel.appendChild(divContainerImages);
-    //addRightArrow(categoryFilms);
-    //console.log('divCarrousel : ', divCarrousel);
-    //console.log('divContainer : ', divContainerImages);
     let position = divCarrousel.getAttribute('position');
     const numberImage = divCarrousel.getAttribute('imageNumber');
     //Get every information for Carrousel management
@@ -305,8 +262,8 @@ function createContainerDiv(categoryFilms, videoTitle) {
 function createImage(categoryFilms, filmData='', indexList) {
     //create a div to display image film, get data to exploit in modal view
 
-    const modalDiv = document.getElementById("image-modal"); //Select Div Modal
-    const imageInModal = document.getElementById("modal-image"); //Select image inside Modal 
+    const modalDiv = document.getElementById("modalDiv"); //Select Div Modal
+    const imageInModal = document.getElementById("modalImageDiv"); //Select image inside Modal 
 
     //At first, documents hierarchy structure creation
     if (document.getElementById(`carrousel${categoryFilms}`) == null) {
@@ -315,6 +272,8 @@ function createImage(categoryFilms, filmData='', indexList) {
     if (document.getElementById(`container${categoryFilms}`) == null) {
         createContainerDiv(categoryFilms, filmData.title);
     };
+    modalClose();
+
     let divContainerImages = document.getElementById(`container${categoryFilms}`);
     //Div creation and image add
     let imageNewDiv = document.createElement('div');
@@ -335,10 +294,12 @@ function createImage(categoryFilms, filmData='', indexList) {
         
     };
     divContainerImages.appendChild(imageNewDiv);
-    modalCreation(imageNewDiv, filmData)
+    modalCreate(imageNewDiv, filmData)
 };
 
 async function getData(url) {
+    // request ur to get data information
+
     try {
         let response = await fetch(url, );
         if (!response.ok) {
@@ -351,8 +312,13 @@ async function getData(url) {
     };
 };
 
+//Main program part
+
 let url = `${urlHref}${titlesPathEndPoint}?sort_by=-imdb_score`;
 console.log('start');
+//hide earlier Modal construction
+let hideModal = document.getElementById('modalDiv');
+hideModal.style.display = "none";
 let bestFilm = getData(url);
 bestFilm.then((jsonData) => {
     let nextUrl = jsonData.next;
@@ -360,18 +326,14 @@ bestFilm.then((jsonData) => {
     let detailedUrl = `${urlHref}${titlesPathEndPoint}${dataArray[0].id}`;
     let detailedBestFilm = getData(detailedUrl);
     detailedBestFilm.then((detailedData) => {
-        //let detailedJsonData = jsonData;
         console.log('jsonDetailedData : ', detailedData);
-        //console.log('titre du film :', detailedDataArray.title);
         createImage('numberOne', detailedData);
     });    
     
-    //console.log('dataArray0 :', dataArray);
     for (index = 1; index < dataArray.length; index++) {
         detailedUrl = `${urlHref}${titlesPathEndPoint}${dataArray[index].id}`;
         detailedBestFilm = getData(detailedUrl);
         detailedBestFilm.then((detailedData) => {
-            //let detailedJsonData = jsonData;
             createImage('bestFilms', detailedData);
         });        
     };
@@ -380,7 +342,6 @@ bestFilm.then((jsonData) => {
         let dataArray = jsonData.results;
         for (index = 0; index < numberOfFilms-4; index++) {
             createImage('bestFilms', dataArray[index]);
-            //console.log('display image : ', dataArray);
         };
     displayHideArrows(0, numberOfFilms, 'bestFilms');
     });
@@ -394,16 +355,12 @@ for (indexList = 2; indexList < categoryList.length; indexList++) {
     dataFilm.then((jsonData) => {
         let nextUrl = jsonData.next;
         let dataArray = jsonData.results;
-        //console.log('jsonData',  dataArray);
         for (index = 0; index < dataArray.length; index++) {
             let detailedUrl = `${urlHref}${titlesPathEndPoint}${dataArray[index].id}`;
             let detailedFilm = getData(detailedUrl);
             detailedFilm.then((detailedData) => {
-                //let detailedJsonData = jsonData;
                 createImage(category, detailedData);
             });        
-        //createImage(category, dataArray[index]);
-            //console.log('display image : ', dataArray);
         };
         let film = getData(nextUrl);
         film.then((jsonData) => {
@@ -412,15 +369,11 @@ for (indexList = 2; indexList < categoryList.length; indexList++) {
                 let detailedUrl = `${urlHref}${titlesPathEndPoint}${dataArray[index].id}`;
                 let detailedFilm = getData(detailedUrl);
                 detailedFilm.then((detailedData) => {
-                    //let detailedJsonData = jsonData;
                     createImage(category, detailedData);
                 });
             
     displayHideArrows(0, numberOfFilms, category);
             };
         });
-        //hide left arrow at first launch
-        
     });
 };
-
